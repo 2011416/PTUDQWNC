2028a472f49562a4b48dca5d03e68e9f8dc6718e
2011416_nhan <2011416@dlu.edu.vn>
2023-04-24 16:15:48 +0700

    Setup App Layout

--- src/furniture-shop/src/components/Layout.jsx
+++ src/furniture-shop/src/components/Layout.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import { BrowserRouter} from 'react-router-dom'
+
+import Header from './Header'
+import Footer from './Footer'
+
+import Switchs from '../routes/Switchs'
+
+
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <BrowserRouter>
+      <div>
+        <div className="container">
+          <Header/>
+          <div className="main">
+            <Switchs/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    </BrowserRouter>      
   )
 }
 
