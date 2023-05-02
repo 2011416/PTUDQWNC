cb16bc2ae9336f70d10ea2425a3d5d0c619452f7
2011416_nhan <2011416@dlu.edu.vn>
2023-05-02 21:23:20 +0700

    fix ViewProduct error

--- src/furniture-shop/src/pages/ViewProduct.jsx
+++ src/furniture-shop/src/pages/ViewProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 import Helmet from '../components/Helmet'
@@ -8,8 +8,13 @@ import ProductCard from '../components/ProductCard'
 import ProductView from '../components/ProductView'
 
 import productData from '../assets/fake-data/product'
+import { getProducts, getProductFilter } from '../Services/Repository'
 
 function ViewProduct(){
+
+    const [productsList, setProductsList] = useState([]);
+    const [isVisibleLoading, setIsVisibleLoading] = useState(true);
+    let k = "", p = 1, ps = 3;
 
     const {slug} =useParams();
 
@@ -21,6 +26,7 @@ function ViewProduct(){
         window.scrollTo(0,0)
     }, [product])
 
+    if (productsList.length > 0)
     return (
         <Helmet title={product.title}>
             <Section>
@@ -40,21 +46,57 @@ function ViewProduct(){
                         gap={20}
                     >
                         {
-                            relatedProducts.map((item, index) => (
+                            productsList.map((item, index) => (
                                 <ProductCard
                                     key={index}
-                                    img01={item.image01}
-                                    img02={item.image02}
-                                    name={item.title}
+                                    img01={item.urlImage}
+                                    img02={item.urlImage}
+                                    name={item.name}
                                     price={Number(item.price)}
-                                    slug={item.slug}
-                                />   
+                                    slug={item.urlSlug}
+                                />
                             ))
                         }
                     </Grid>
                 </SectionBody>
             </Section>
         </Helmet>
+    );
+    else return 
+    (
+        <Helmet title={product.title}>
+        <Section>
+            <SectionBody>
+                <ProductView product={product}/>
+            </SectionBody>
+        </Section>
+        <Section>
+            <SectionTitle>
+                Khám phá thêm
+            </SectionTitle>
+            <SectionBody>
+                <Grid
+                    col={4}
+                    mdCol={2}
+                    smCol={1}
+                    gap={20}
+                >
+                    {
+                        relatedProducts.map((item, index) => (
+                            <ProductCard
+                                key={index}
+                                img01={item.image01}
+                                img02={item.image02}
+                                name={item.title}
+                                price={Number(item.price)}
+                                slug={item.slug}
+                            />   
+                        ))
+                    }
+                </Grid>
+            </SectionBody>
+        </Section>
+    </Helmet>
     )
 }
 
